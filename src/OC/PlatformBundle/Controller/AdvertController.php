fa353698822dd32b7d1099c1da3542c29c5bf7d4
Eric Payet-Desruisseaux <Eric Payet-Desruisseaux>
2016-07-06 17:34:18 +0200

    test modif

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -2,32 +2,47 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class AdvertController extends Controller {
+class AdvertController extends Controller
+{
 
-    public function indexAction() {
-        $url = $this->get('router')->generate(
-            'oc_platefrom_view', array('id' => 5)
-        );
-
-        return new Response("L'URL de l'annonce d'Id 5 est : " . $url);
+    public function indexAction($page)
+    {
+        if ($page < 1){
+            throw new NotFoundHttpException('Page "'.$page.'"Inexistante.');
+        }
     }
 
-    public function testAction() {
-        $content = $this->get('templating')->render('PlatformBundle:Advert:test.html.twig', array('nom' => 'Aimrys'));
+    public function testAction()
+    {
+        $content = $this->get('templating')->render('OCPlatformBundle:Advert:test.html.twig', array('nom' => 'Aimrys'));
         return new Response($content);
     }
 
     public function viewAction($id)
     {
-        $url = $this->get('router')->generate('oc_platform_home');
-
-        return new RedirectResponse($url);
+        return $this->render('OCPlatformBundle:Advert:view.html.twig', array('id' => $id));
     }
 
-    public function viewSlugAction($slug, $year, $format) {
+    public function addAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+
+        $session->getFlashBag()->add('info', 'Oui oui elle est bien enregistrée !');
+
+        return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+    }
+
+
+    public function viewSlugAction($slug, $year, $format)
+    {
         return new Response(
             "On pourrait afficher l'annonce correspondant au slug '" . $slug . "', créée en " . $year . " et au format " . $format . "."
         );
